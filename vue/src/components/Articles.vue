66edf138e6afacb890f8a56c38ba5ea038a4e3b1
György Mátyás <gy.matyas97@gmail.com>
2018-05-07 11:05:30 +0200

    added component for article

--- vue/src/components/Articles.vue
+++ vue/src/components/Articles.vue
@@ -1,5 +1,12 @@
 <template>
-
+  <div class="article-custom">
+    <div class="media d-inline-block" style="padding:20px;vertical-align:top;text-align:center;"><img class="img-fluid" src="static/img/Beautiful-Wallpaper.jpg" style="padding:0px 0px 30px 0px;">
+      <div class="media-body d-block">
+        <h5 class="article-header-custom">Media Heading</h5>
+        <p class="article-text-custom">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus nisl ac diam feugiat, non vestibulum libero posuere. Vivamus pharetra leo non nulla egestas, nec malesuada orci finibus. </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
