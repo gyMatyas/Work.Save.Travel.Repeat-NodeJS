9c44fef49873e6b5d6c2da3efd78d2dc041ed84c
György Mátyás <gy.matyas97@gmail.com>
2018-05-07 14:56:59 +0200

    oauth added

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,67 @@
 <template>
   <div id="app">
+    <div>
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Auth0 - Vue</a>
+
+            <router-link :to="'/'"
+                         class="btn btn-primary btn-margin">
+              Home
+            </router-link>
+
+            <button
+              class="btn btn-primary btn-margin"
+              v-if="!authenticated"
+              @click="login()">
+              Log In
+            </button>
+
+            <button
+              class="btn btn-primary btn-margin"
+              v-if="authenticated"
+              @click="logout()">
+              Log Out
+            </button>
+
+          </div>
+        </div>
+      </nav>
+
+      <div class="container">
+        <router-view
+          :auth="auth"
+          :authenticated="authenticated">
+        </router-view>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
+
+import AuthService from './auth/AuthService'
+const auth = new AuthService()
+
+const { login, logout, authenticated, authNotifier } = auth
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    authNotifier.on('authChange', authState => {
+      this.authenticated = authState.authenticated
+    })
+    return {
+      auth,
+      authenticated
+    }
+  },
+  methods: {
+    login,
+    logout
+  }
 }
 </script>
 
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Articles from '@/components/Articles'
 import AboutUs from '@/components/AboutUs'
+import Callback from '@/components/Callback'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: "/aboutUs",
       name: "AboutUs",
       component: AboutUs
+    },
+    {
+      path: "/callback",
+      name: "Callback",
+      component: Callback
     }
   ]
 })
