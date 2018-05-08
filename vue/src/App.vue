43904ed23851904a4f8cfaa64ff8a2b8b2a5dc4a
György Mátyás <gy.matyas97@gmail.com>
2018-05-08 09:56:19 +0200

    fixed login

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,42 +1,41 @@
 <template>
   <div id="app">
     <div>
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#">Auth0 - Vue</a>
+      <nav class="navbar navbar-light navbar-expand-md sticky-top order-2 navbar-dark" style="position:fixed;width:100%;">
+        <div class="container-fluid"><a class="navbar-brand" href="/"><img class="img-fluid"
+                                                                            src="static/img/inverted_logo.png" width="50vw"
+                                                                            id="logo"
+                                                                            style="padding:0;margin:15px 15px;"><strong
+          class="d-none d-sm-inline d-md-inline d-lg-inline d-xl-inline">Work. Save. Travel. Repeat.</strong></a>
+          <button
+            class="navbar-toggler float-left" data-toggle="collapse" data-target="#navcol-1"
+            style="position:fixed;top:10px;right:5px;"><span class="sr-only">Toggle navigation</span><span
+            class="navbar-toggler-icon custom-toggler"></span></button>
+          <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav">
+              <li class="nav-item" role="presentation"><a class="nav-link active" href="/aboutUs">About us</a></li>
+              <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Travels</a></li>
+              <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Contact</a></li>
+              <li
+                class="nav-item" role="presentation"
+                v-if="!authenticated">
+                <a class="nav-link active pointer" @click="login()">Log In</a>
+              </li>
 
-            <router-link :to="'/'"
-                         class="btn btn-primary btn-margin">
-              Home
-            </router-link>
-
-            <button
-              class="btn btn-primary btn-margin"
-              v-if="!authenticated"
-              @click="login()">
-              Log In
-            </button>
-
-            <button
-              class="btn btn-primary btn-margin"
-              v-if="authenticated"
-              @click="logout()">
-              Log Out
-            </button>
-
+              <li
+                class="nav-item" role="presentation"
+                v-if="authenticated">
+                <a class="nav-link active pointer" @click="logout()">Log Out</a>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
-
-      <div class="container">
         <router-view
           :auth="auth"
           :authenticated="authenticated">
         </router-view>
-      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -73,5 +72,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.pointer {
+  cursor: pointer;
 }
 </style>
--- vue/src/auth/AuthService.js
+++ vue/src/auth/AuthService.js
@@ -20,7 +20,7 @@ export default class AuthService {
     redirectUri: 'http://localhost:60000/callback',
     audience: 'https://wstr.eu.auth0.com/userinfo',
     responseType: 'token id_token',
-    scope: 'openid'
+    scope: 'openid profile'
   })
 
   login () {
@@ -31,9 +31,10 @@ export default class AuthService {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult)
-        router.replace('home')
+        console.log(authResult);
+        router.push('/')
       } else if (err) {
-        router.replace('home')
+        router.replace('/')
         console.log(err)
       }
     })
@@ -58,7 +59,7 @@ export default class AuthService {
     this.userProfile = null
     this.authNotifier.emit('authChange', false)
     // navigate to the home route
-    router.replace('home')
+    router.replace('/')
   }
 
   isAuthenticated () {
